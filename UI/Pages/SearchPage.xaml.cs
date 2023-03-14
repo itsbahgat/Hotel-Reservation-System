78d12210fbeabeea57c6158eb197f6e526c7eb91
Youssef king <yussefbahgat@gmail.com>
2023-03-14 08:51:27 +0200

    Add initial search functionality and animation triggers

--- UI/Pages/SearchPage.xaml.cs
+++ UI/Pages/SearchPage.xaml.cs
@@ -12,15 +12,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DataAccess;
+using DataAccess.Entities;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Animation;
 
 namespace HotelManagementSystem.Pages
 {
 
     public partial class SearchPage : Page
     {
+
+        private AppDbContext _context = new AppDbContext();
+        private bool isAnimationTriggered = false;
+
+
         public SearchPage()
         {
             InitializeComponent();
+
+
+        }
+
+        private void SearchTermTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchTermTextBox.Text.Length > 0 && !isAnimationTriggered)
+            {
+                Storyboard moveUpStoryboard = (Storyboard)FindResource("MoveUpStoryboard");
+                moveUpStoryboard.Begin();
+                isAnimationTriggered = true;
+                LoadingIndicator.Visibility = Visibility.Visible;
+
+            }
+
+            if (SearchTermTextBox.Text.Length == 0 && isAnimationTriggered)
+            {
+                ReservationList.ItemsSource = null;
+                ReservationList.Visibility = Visibility.Hidden;
+                LoadingIndicator.Visibility = Visibility.Hidden;
+
+
+
+                Storyboard MoveDownStoryboard = (Storyboard)FindResource("MoveDownStoryboard");
+                MoveDownStoryboard.Begin();
+                isAnimationTriggered = false;
+
+            }
+
+        }
+
+        private void MoveUpStoryboard_Completed(object sender, EventArgs e)
+        {
+            FillDataGrid();
+        }
+
+
+        private void FillDataGrid()
+        {
+          //@TODO
         }
     }
 }
