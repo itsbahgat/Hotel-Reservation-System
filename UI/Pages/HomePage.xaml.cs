a9a0168a302c9b7cfa2773f49c5c78d8d742951d
Youssef king <yussefbahgat@gmail.com>
2023-03-17 15:32:50 +0200

    Add sumbit initial implementation

--- UI/Pages/HomePage.xaml.cs
+++ UI/Pages/HomePage.xaml.cs
@@ -18,9 +18,37 @@ namespace HotelManagementSystem.Pages
 
     public partial class HomePage : Page
     {
+        private AppDbContext _context = new AppDbContext();
+
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void sumbitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            _context.Reservations.Add(new Reservation { FirstName = firstName.Text,
+                                                        LastName = lastName.Text,
+                                                        BirthDay = birthdate.Text,
+                                                        Gender = gender.Text,
+                                                        PhoneNumber = phone.Text,
+                                                        EmailAddress = email.Text,
+                                                        StreetAddress = stAddress.Text,
+                                                        AptSuite = apartment.Text,
+                                                        City = city.Text,
+                                                        State = governorate.Text,
+                                                        ZipCode = zipCode.Text,
+                                                        RoomFloor = floorNum.Text,
+                                                        RoomNumber = roomNum.Text,
+                                                        NumberGuest = Convert.ToInt32(guestNum.Text),
+                                                        RoomType = roomType.Text,
+                                                        ArrivalTime= Convert.ToDateTime(arrivalDate.Text),
+                                                        LeavingTime= Convert.ToDateTime(leavingDate.Text),
+                                                   });
+
+            _context.SaveChanges();
+
         }
     }
 }
