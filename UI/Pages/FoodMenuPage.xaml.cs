6fcff6af707f61055276235a73142d3a878ea071
Youssef king <yussefbahgat@gmail.com>
2023-03-20 07:11:13 +0200

    Add basic functionality to FoodMenuPage

--- UI/Pages/FoodMenuPage.xaml.cs
+++ UI/Pages/FoodMenuPage.xaml.cs
@@ -15,11 +15,54 @@ using System.Windows.Shapes;
 
 namespace HotelManagementSystem.Pages
 {
-    public partial class FoodMenuPage : Page
+    public partial class FoodMenuPage : Window
     {
         public FoodMenuPage()
         {
             InitializeComponent();
         }
+        
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void EnableBreakfastTextBox(object sender, RoutedEventArgs e)
+        {
+            breakfastTxt.IsReadOnly = false;
+        }
+
+        private void DisableBreakfastTextBox(object sender, RoutedEventArgs e)
+        {
+            breakfastTxt.Text = string.Empty;
+            breakfastTxt.IsReadOnly = true;
+        }
+
+        private void EnableDinnerTextBox(object sender, RoutedEventArgs e)
+        {
+            dinnerTxt.IsReadOnly = false;
+
+        }
+
+        private void DisableDinnerTextBox(object sender, RoutedEventArgs e)
+        {
+            dinnerTxt.Text = string.Empty;
+            dinnerTxt.IsReadOnly = true;
+
+        }
+
+        private void EnableLunchTextBox(object sender, RoutedEventArgs e)
+        {
+            lunchTxt.IsReadOnly = false;
+        }
+
+        private void DisableLunchTextBox(object sender, RoutedEventArgs e)
+        {
+            lunchTxt.Text = string.Empty;
+            lunchTxt.IsReadOnly = true;
+    }
+
     }
 }
