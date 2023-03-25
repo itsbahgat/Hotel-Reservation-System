f616c24be51ca26c32f601c1e28602dfe2324237
Youssef king <yussefbahgat@gmail.com>
2023-03-25 22:08:14 +0200

    Add list of room numbers.

--- UI/Pages/HomePage.xaml.cs
+++ UI/Pages/HomePage.xaml.cs
@@ -29,7 +29,9 @@ namespace HotelManagementSystem.Pages
         {
             InitializeComponent();
 
-            ReservationComboBox.ItemsSource = _context.Reservations.Select(r => $" {r.Id} | {r.FirstName} {r.LastName} | {r.PhoneNumber}" ).ToList(); ;
+            roomNum.ItemsSource = new List<string>() { "101", "102", "103", "104", "105", "106", "107", "108", "109", "110",
+            "201", "202", "203", "204", "205", "206", "207", "208", "209", "210", "301", "302", "303", "304", "305", "306", "307", "308", "309", "310",
+            "401", "402", "403", "404", "405", "406", "407", "408", "409", "410", "501", "502", "503", "504", "505", "506", "507", "508", "509", "510"};
 
         }
 
