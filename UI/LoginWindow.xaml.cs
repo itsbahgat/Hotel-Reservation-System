5401c0bd22a182f6e0108518dba4d8304916c503
Youssef king <yussefbahgat@gmail.com>
2023-04-10 07:47:53 +0200

    Add kitchen user role

--- UI/LoginWindow.xaml.cs
+++ UI/LoginWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace HotelManagementSystem
 {
     public partial class LoginWindow : Window
     {
+        bool kitchen = false;
 
         public LoginWindow()
         {
@@ -40,6 +41,8 @@ namespace HotelManagementSystem
                 if (isAuthenticatedUser())
                 {
                     MainWindow mainWindow = new MainWindow();
+                if (kitchen)
+                    mainWindow.ShowKitchenButtons();
                     mainWindow.Show();
                     Close();
                 }
@@ -56,11 +59,15 @@ namespace HotelManagementSystem
             {
                 var username = txtUsername.Text;
                 var password = txtPassword.Password;
-
                 var login = context.Logins.SingleOrDefault(l => l.username == username && l.password == password);
-
+                if(login !=null && login.username =="kitchen")
+                {
+                    kitchen = true;
+                }
                 return login != null;
             }
         }
+
+       
     }
 }
